803a69238a217007318f9514291e95012d07ce5f
superduperdc <dave@crashboombang.com>
2024-12-29 09:32:35 -0800

    fetch genre

---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { Text } from "@chakra-ui/react";
+import { CanceledError } from "axios";
+
+interface Genre {
+    id: number;
+    name: string;
+}
+
+interface FetchGenresResponse {
+    count: number;
+    results: Genre[];
+}
+
+const useGenres = () => {
+    const [genres, setGenres] = useState<Genre[]>([]);
+    const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+    apiClient
+      .get<FetchGenresResponse>("/genres", { signal: controller.signal })
+      .then((res) => setGenres(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+
+    return () => controller.abort(); // Cleanup function
+  }, []);
+
+  return { genres, error };
+};
+
+export default useGenres;
