5f1cbd2ca4872865a8b216386dc1a1d2434ed5f8
superduperdc <dave@crashboombang.com>
2024-12-28 19:08:56 -0800

    Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <Button
-      colorScheme="blue"
-      variant="outline"
-      onClick={() => setCount(count + 1)}
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
     >
-      Click
-    </Button>
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
